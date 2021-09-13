2a4921ee93d6ea9575bfd04b5bba5cecba5e8eb2
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-09-13 18:57:56 +0200

    API: Fix booking notification AR

--- database/seeds/BookingStatusesTableSeeder.php
+++ database/seeds/BookingStatusesTableSeeder.php
@@ -26,7 +26,7 @@ class BookingStatusesTableSeeder extends Seeder
             0 =>
                 array(
                     'id' => 1,
-                    'status' => 'Received',
+                    'status' => 'تم الاستلام',
                     'order' => 1,
                     'created_at' => '2021-01-25 19:25:46',
                     'updated_at' => '2021-01-29 17:56:35',
@@ -34,7 +34,7 @@ class BookingStatusesTableSeeder extends Seeder
             1 =>
                 array(
                     'id' => 2,
-                    'status' => 'In Progress',
+                    'status' => 'في تقدم',
                     'order' => 40,
                     'created_at' => '2021-01-25 19:26:02',
                     'updated_at' => '2021-02-16 21:56:52',
@@ -42,7 +42,7 @@ class BookingStatusesTableSeeder extends Seeder
             2 =>
                 array(
                     'id' => 3,
-                    'status' => 'On the Way',
+                    'status' => 'علي الطريق',
                     'order' => 20,
                     'created_at' => '2021-01-28 07:47:23',
                     'updated_at' => '2021-02-16 12:10:13',
@@ -50,7 +50,7 @@ class BookingStatusesTableSeeder extends Seeder
             3 =>
                 array(
                     'id' => 4,
-                    'status' => 'Accepted',
+                    'status' => 'تم الموافقة',
                     'order' => 10,
                     'created_at' => '2021-02-16 12:09:29',
                     'updated_at' => '2021-02-16 12:10:06',
@@ -58,7 +58,7 @@ class BookingStatusesTableSeeder extends Seeder
             4 =>
                 array(
                     'id' => 5,
-                    'status' => 'Ready',
+                    'status' => 'مستعد',
                     'order' => 30,
                     'created_at' => '2021-02-16 12:11:50',
                     'updated_at' => '2021-02-16 21:56:42',
@@ -66,7 +66,7 @@ class BookingStatusesTableSeeder extends Seeder
             5 =>
                 array(
                     'id' => 6,
-                    'status' => 'Done',
+                    'status' => 'منتهي',
                     'order' => 50,
                     'created_at' => '2021-02-16 21:57:02',
                     'updated_at' => '2021-02-16 21:57:02',
@@ -74,7 +74,7 @@ class BookingStatusesTableSeeder extends Seeder
             6 =>
                 array(
                     'id' => 7,
-                    'status' => 'Failed',
+                    'status' => 'باءت بالفشل',
                     'order' => 60,
                     'created_at' => '2021-02-16 21:58:36',
                     'updated_at' => '2021-02-16 21:58:36',
